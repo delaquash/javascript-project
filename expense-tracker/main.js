e8c7340af4971740785093d5062a665c5c3e3432
delaquash <olaide1191@gmail.com>
2022-02-11 19:45:02 +0100

    implemented DOM functionalities for expense-tracker

---
+++ expense-tracker/main.js
@@ -0,0 +1,31 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount')
+
+
+const dummyTransaction = [
+    {
+        id: 1,
+        text: 'flower',
+        amount: -20
+    }, 
+    {
+        id: 2,
+        text: 'Salary',
+        amount: 300
+    },
+    {
+        id: 3,
+        text: 'Book',
+        amount: 10
+    },
+    {
+        id: 4,
+        text: 'Camera',
+        amount: 150
+    }
+]
