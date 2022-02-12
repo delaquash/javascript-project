863b03914213c78aadbbf6174a7c7e78f28b47ce
delaquash <olaide1191@gmail.com>
2022-02-12 06:42:16 +0100

    implemented show transaction amount in expense tracker app

--- expense-tracker/main.js
+++ expense-tracker/main.js
@@ -29,3 +29,36 @@ const dummyTransaction = [
         amount: 150
     }
 ]
+
+let transactions = dummyTransaction
+
+// add transactions to DOM
+const addTransactionToDom = (transactions) => {
+    // declare sign
+    const sign = transactions.amount < 0 ? '-' : '+';
+    const item = document.createElement('li');
+
+    // add class based on value
+    item.classList.add(transactions.amount < 0 ? 'minus' : 'plus')
+
+    item.innerHTML = `
+        ${transactions.text}<span>${sign} ${Math.abs(transactions.amount)}</span>
+        <button class="delete-btn">X</button>`;
+
+        list.appendChild(item)
+}
+
+// update the balance, income and expense
+function updateValues () {
+    const amounts = transactions.map( transaction => transaction.map)
+}
+
+
+
+// init app
+const init = () => {
+    list.innerHTML = '';
+    transactions.forEach(addTransactionToDom)
+}
+
+init()
