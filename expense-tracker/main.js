a4d9d08acb44ad51ae6ff125d2475fd76dedb34c
delaquash <olaide1191@gmail.com>
2022-02-13 19:22:16 +0100

    implemented localStorage for data storage

--- expense-tracker/main.js
+++ expense-tracker/main.js
@@ -7,43 +7,78 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount')
 
 
-const dummyTransaction = [
-    {
-        id: 1,
-        text: 'flower',
-        amount: -20
-    }, 
-    {
-        id: 2,
-        text: 'Salary',
-        amount: 300
-    },
-    {
-        id: 3,
-        text: 'Book',
-        amount: 10
-    },
-    {
-        id: 4,
-        text: 'Camera',
-        amount: 150
-    }
-]
+// const dummyTransaction = [
+//     {
+//         id: 1,
+//         text: 'flower',
+//         amount: -20
+//     }, 
+//     {
+//         id: 2,
+//         text: 'Salary',
+//         amount: 300
+//     },
+//     {
+//         id: 3,
+//         text: 'Book',
+//         amount: 10
+//     },
+//     {
+//         id: 4,
+//         text: 'Camera',
+//         amount: 150
+//     }
+// ]
 
-let transactions = dummyTransaction
+const localStorageTransaction = JSON.parse(localStorage.getItem('transactions'))
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransaction : []
+
+const addTransaction=(e) => {
+    e.preventDefault()
+
+    if(amount.value.trim() && text.value.trim() === '') {
+        alert('Please add amount and value')
+    } else {
+        const transaction = {
+            id: generateID,
+            text: text.value,
+            // the + sign is to make amount which is a string to become a number
+            amount: +amount.value
+        }
+        transactions.push(transaction)
+        addTransactionToDom(transaction)
+        updateValues()
+        updateLocalStorage()
+        // to clear the input form after submission of values
+        text.value ='';
+        amount.value='';
+    }
+
+}
+
+// generate random ID
+
+function generateID() {
+    return Math.floor(Math.random() * 100000000)
+}
 
 // add transactions to DOM
-const addTransactionToDom = (transactions) => {
+const addTransactionToDom = (transaction) => {
     // declare sign
-    const sign = transactions.amount < 0 ? '-' : '+';
+    const sign = transaction.amount < 0 ? '-' : '+';
     const item = document.createElement('li');
 
     // add class based on value
-    item.classList.add(transactions.amount < 0 ? 'minus' : 'plus')
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
 
     item.innerHTML = `
-        ${transactions.text}<span>${sign} ${Math.abs(transactions.amount)}</span>
-        <button class="delete-btn">X</button>`;
+        ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}</span>
+        <button 
+            onclick="removeTransaction(${transaction.id})
+        class="delete-btn">
+        x
+        </button>`;
 
         list.appendChild(item)
 }
@@ -71,6 +106,18 @@ function updateValues () {
     money_minus.innerHTML = `$${expenses}`
 }
 
+// remove a particular transaction via ID
+const removeTransaction = (id) => {
+    transactions = transactions.filter(transaction => transaction.id !== id)
+    updateLocalStorage();
+    init()
+}
+
+// update localStorage to set value
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
 
 
 // init app
@@ -81,3 +128,5 @@ const init = () => {
 }
 
 init()
+
+form.addEventListener('submit', addTransaction)
