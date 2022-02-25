a561a6b9165bf58858997bece61353e044eb1d1d
delaquash <olaide1191@gmail.com>
2022-02-25 10:56:16 +0100

    implemented cors for lyrics search app

--- lyrics-search-app/main.js
+++ lyrics-search-app/main.js
@@ -3,7 +3,7 @@ const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
 
-const apiURL = 'https://api.lyrics.ovh';
+const apiURL = 'https://api.lyrics.ovh'
 
 // fetch songs
 async function searchSongs(term) {
@@ -14,7 +14,46 @@ async function searchSongs(term) {
 
 // to search for song
 function showData(data) {
-    
+  
+    result.innerHTML = `
+    <ul class="songs>
+        ${data.data.map(song => 
+            `<li>
+                <span><strong>${song.artist.name}</strong> -${song.title}</span>
+                <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">
+                    Get Lyrics                
+                </button>            
+            </li>`
+            // to turn from array to a string
+        ).join('')}
+    </ul>
+    `;
+
+    // pagination----previous and next
+    if(data.prev || data.next) {
+        more.innerHTML = `
+        ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')")>Prev</button>`:''}
+        ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.prev}')")>Next</button>`:''}
+        `
+    } else{
+        more.innerHTML ='';
+    }
+}
+
+// prev and next song
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${apiURL}`)
+    const data = await res.json();
+
+    showData(data)
+}
+
+// get lyrics for song
+async function getLyrics(artist, songTitle){
+    const res = await fetch(`${apiURL}/v2/${artist}/${songTitle}`)
+
+    const data = res.json();
+    console.log(data)
 }
 
 // event listeners
@@ -30,3 +69,16 @@ form.addEventListener('submit', e => {
     }
 })
 
+// get lyrics from button
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if(clickedEl.tagName === 'BUTTON'){
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle'); 
+        
+        getLyrics(artist, songTitle)
+    }
+
+   
+})
