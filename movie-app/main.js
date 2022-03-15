8eee2ba10c40de01c374a062b352832cd7013b8b
delaquash <olaide1191@gmail.com>
2022-03-15 17:18:05 +0100

    implemented adding movies to the DCM

--- movie-app/main.js
+++ movie-app/main.js
@@ -1,5 +1,6 @@
 const search = document.getElementById('search');
 const form = document.getElementById('form');
+const main = document.getElementById('main');
 
 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
@@ -12,7 +13,41 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach(movie => {
+        const { title, poster_path, overview, vote_average } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>
+    `
+    main.appendChild(movieEl)
+        
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8){
+        return 'green'
+    } else if (vote >= 5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 
@@ -25,7 +60,6 @@ form .addEventListener('submit', (e)=> {
         getMovies(SEARCH_API + searchTerm)
         search.value=''
     } else {
-        
         window.location.reload()
     }
 })
